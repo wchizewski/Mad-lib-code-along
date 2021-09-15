7225f5ad601116c2f25ff9a899019a44a47e250d
wchizewski <w.chizewski@share.epsb.ca>
2021-09-15 14:18:25 -0600

    finishing mad lib js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-// Mad lib with js by will c
+// Mad lib with js by Will
 
-// 
+// Event listener
+document.getElementById("madBtn").addEventListener("click", buildMadLib);
+
+// Event Function
+function buildMadLib() {
+    // Input
+    let pluralNoun = document.getElementById("pluralNoun").value; 
+    let adjective = document.getElementById("adjective").value;
+    let presentTenseVerb = document.getElementById("presentTenseVerb").value;
+    let noun = document.getElementById("noun").value;
+
+    // Process
+    let result = '"There are too many ' + pluralNoun + ' on this ' + adjective + ' airplane!", I screamed. "Somebody has to ' + presentTenseVerb + ' on the ' + noun + ' to solve this problem!"'
+
+    // Output
+    document.getElementById("result").innerHTML = result;
+
+
+}
+
